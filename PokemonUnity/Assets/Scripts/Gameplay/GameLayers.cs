1a73ef237337d4d3319929311cddbb0c60a498e9
Matheus Freitas Fernandes <matheusfreitasfernandes18@gmail.com>
2025-09-04 02:51:08 -0300

    feat: Sistema de Animações: SpriteAnimator & CharacterAnimator
    ✅ Identificação de limitações do sistema de animação nativo da Unity;
    ✅ Criação da classe SpriteAnimator para gerenciar animações via sprites;
    ✅ Teste do SpriteAnimator animando um NPC;
    ✅ Implementação da classe CharacterAnimator para controlar diferentes estados de animação;
    ✅ Animação do player usando o CharacterAnimator personalizado;
    ✅ Correção de casos extremos (edge cases) no fluxo de animação.
    ✅ Separação de comportamentos comuns em um script Character;
    ✅ Implementação da ação OnMoveOver para executar eventos após o movimento;
    ✅ Criação do script GameLayers para organizar as layers;
    ✅ Limpeza e simplificação do PlayerController;
    ✅ Uso do script Character para movimentar o Player;
    ✅ Implementação de movimentação para NPCs;
    ✅ Caminhada dos NPCs em padrões predefinidos;
    ✅ Bloqueio da movimentação de NPCs durante diálogos.

---
+++ PokemonUnity/Assets/Scripts/Gameplay/GameLayers.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLayers : MonoBehaviour
+{
+    [SerializeField] public LayerMask solidObjectsLayer;
+    [SerializeField] public LayerMask interactableLayer;
+    [SerializeField] public LayerMask grassLayer;
+
+    public static GameLayers i { get; set; }
+    private void Awake()
+    {
+        i = this;
+    }
+
+    public LayerMask SolidLayer
+    {
+        get => solidObjectsLayer;
+    }
+
+    public LayerMask InteractableLayer
+    {
+        get => interactableLayer;
+    }
+
+    public LayerMask GrassLayer
+    {
+        get => grassLayer;
+    }
+}
