b53173ab4803390ff1d5ea99b8a5baa0355593fb
Matheus Freitas Fernandes <matheusfreitasfernandes18@gmail.com>
2025-10-10 04:01:01 -0300

    feat: Sistema de Coleta de Itens (Pickups)
    ✅ Implementação do sistema de coleta de itens (Pickups) no mundo do jogo;
    ✅ Correção do problema na função ShowDialogText, que afetava a exibição de textos de diálogo;
    ✅ Integração dos pickups com o inventário do jogador;
    ✅ Feedback visual e textual ao coletar um item;
    ✅ Testes de funcionamento em diferentes tipos de itens e cenários.

--- PokemonUnity/Assets/Scripts/Inventory/Inventory.cs
+++ PokemonUnity/Assets/Scripts/Inventory/Inventory.cs
@@ -52,6 +52,28 @@ public class Inventory : MonoBehaviour
         return null;
     }
 
+    public void AddItem(ItemBase item, int count=1)
+    {
+        int category = (int)GetCategoryFromItem(item);
+        var currentSlots = GetSlotsByCategory(category);
+
+        var itemSlot = currentSlots.FirstOrDefault(slot => slot.Item == item);
+        if (itemSlot != null)
+        {
+            itemSlot.Count += count;
+        }
+        else
+        {
+            currentSlots.Add(new ItemSlot()
+            {
+                Item = item,
+                Count = count
+            });
+        }
+
+        OnUpdated?.Invoke();
+    }
+
     public void RemoveItem(ItemBase item, int category)
     {
         var currentSlots = GetSlotsByCategory(category);
@@ -62,6 +84,16 @@ public class Inventory : MonoBehaviour
             currentSlots.Remove(itemSlot);
 
         OnUpdated?.Invoke();
+    }
+
+    ItemCategory GetCategoryFromItem(ItemBase item)
+    {
+        if (item is RecoveryItem)
+            return ItemCategory.Items;
+        else if (item is PokeballItem)
+            return ItemCategory.Pokeballs;
+        else
+            return ItemCategory.TMs;
     }
 
     public static Inventory GetInventory()
@@ -76,7 +108,10 @@ public class ItemSlot
     [SerializeField] ItemBase item;
     [SerializeField] int count;
 
-    public ItemBase Item => item;
+    public ItemBase Item {
+        get => item;
+        set => item = value;
+    }
     public int Count { 
         get =>  count;
         set => count = value;
