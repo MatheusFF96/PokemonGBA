d3461d0071c07662a697e5d6397afd1dc0fe2ac0
Matheus Freitas Fernandes <matheusfreitasfernandes18@gmail.com>
2025-10-17 07:49:28 -0300

    feat: Quest

--- PokemonUnity/Assets/Scripts/Quest/Quest.cs
+++ PokemonUnity/Assets/Scripts/Quest/Quest.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
 public class Quest
 {
     public QuestBase Base { get; private set; }
@@ -18,9 +17,6 @@ public class Quest
         Status = QuestStatus.Started;
 
         yield return DialogManager.Instance.ShowDialog(Base.StartDialogue);
-
-        var questList = QuestList.GetQuestList();
-        questList.AddQuest(this);
     }
 
     public IEnumerator CompleteQuest(Transform player)
@@ -42,9 +38,6 @@ public class Quest
             string playerName = player.GetComponent<PlayerController>().Name;
             yield return DialogManager.Instance.ShowDialogText($"{playerName} recebeu {Base.RewardItem.Name}.");
         }
-
-        var questList = QuestList.GetQuestList();
-        questList.AddQuest(this);
     }
 
     public bool CanBeCompleted()
